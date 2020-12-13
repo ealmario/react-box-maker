4d4686c8af8a34bee89ca2ca1ea76cf6dc578e8e
ealmario <ee.almario@gmail.com>
2020-12-13 12:52:10 +0800

    Create init box list comp

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Box from './components/Box';
 function App() {
   return (
     <div className="App">
-      <h1>Box Maker</h1>
       <Box width={300} height={150} color="steelblue"/>
+      <Box width={100} height={50} color="purple"/>
     </div>
   );
 }
---
+++ src/components/BoxList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class BoxList extends Component {
+  render() {
+    return (
+      <div className="BoxList">
+        <h1 className="title">Color Box Maker Thingy</h1>
+      </div>
+    )
+  }
+}
+
+export default BoxList;
